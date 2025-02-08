bbbfde36b35d84b03fa956a97cf1d996b7b4cba2
LeBern Matthews <128014055+LeBern-Matthews@users.noreply.github.com>
2025-02-08 17:33:51 -0400

    Initialising stuff and adding everything to the repo. Attempting to be more organised

---
+++ frontend.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+def main():
+    print("Hello from frontend.main.py")
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,7 @@
+import frontend, backend
+
+def main():
+    print("Hello from main.py")
+    frontend.main()
+if __name__ == '__main__':
+    main()
